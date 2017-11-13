b2fb81b30cd2523f04f0b1601c58dd65456b35fc
Daniel Ávila <daniel.avilaa@hotmail.com>
2017-11-13 01:51:27 -0200

    fix: Remove wrong instance

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ import mongoose from 'mongoose'
 import graphqlHTTP from 'express-graphql'
 
 import schema from './source/graphql'
-
 import config from './source/config'
-import UserController from './source/controllers/User'
 
 const app = express()
 const PORT = 8000
@@ -32,7 +30,5 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Server is running!')
 })
-
-app.get('/user', UserController.getUser)
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
